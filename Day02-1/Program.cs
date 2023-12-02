38591a587d1d9821c0725e31cac12fe046055689
Justin Drögemeier <jd@diamant-software.de>
2023-12-02 16:22:21 +0100

    day 02

---
+++ Day02-1/Program.cs
@@ -0,0 +1,46 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var lines = File
+    .ReadAllLines("./input.txt")
+    .Select(l => l.Split(":").Last())
+    .ToArray();
+
+const int maxRed = 12;
+const int maxBlue = 14;
+const int maxGreen = 13;
+
+int result = 0;
+
+for (var index = 0; index < lines.Length; index++)
+{
+    var flag = false;
+    foreach (var set in lines[index].Split(";"))
+    {
+        var colors = set.Split(",");
+        foreach(var color in colors)
+        {
+            var tuple = color.Split(" ");
+            switch (tuple[2].ToLower())
+            {
+                case "red":
+                    if (int.Parse(tuple[1]) > maxRed)
+                        flag = true;
+                    break;
+                case "blue":
+                    if (int.Parse(tuple[1]) > maxBlue)
+                        flag = true;
+                    break;
+                case "green":
+                    if (int.Parse(tuple[1]) > maxGreen)
+                        flag = true;
+                    break;
+            }
+        }
+    }
+
+    if (!flag)
+        result += index + 1;
+}
+
+
+Console.WriteLine(result.ToString());
---
+++ Day02-2/Program.cs
@@ -0,0 +1,50 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var lines = File
+    .ReadAllLines("./input.txt")
+    .Select(l => l.Split(":").Last())
+    .ToArray();
+
+const int maxRed = 12;
+const int maxBlue = 14;
+const int maxGreen = 13;
+
+int result = 0;
+
+for (var index = 0; index < lines.Length; index++)
+{
+    var flag = false;
+    var atLeastRed = int.MinValue;
+    var atLeastGreen = int.MinValue;
+    var atLeastBlue = int.MinValue;
+    
+    foreach (var set in lines[index].Split(";"))
+    {
+        var colors = set.Split(",");
+        foreach(var color in colors)
+        {
+            
+            var tuple = color.Split(" ");
+            switch (tuple[2].ToLower())
+            {
+                case "red":
+                    if (int.Parse(tuple[1]) > atLeastRed)
+                        atLeastRed = int.Parse(tuple[1]);
+                    break;
+                case "blue":
+                    if (int.Parse(tuple[1]) > atLeastBlue)
+                        atLeastBlue = int.Parse(tuple[1]);
+                    break;
+                case "green":
+                    if (int.Parse(tuple[1]) > atLeastGreen)
+                        atLeastGreen = int.Parse(tuple[1]);
+                    break;
+            }
+        }
+    }
+
+    result += atLeastRed * atLeastBlue * atLeastGreen;
+}
+
+
+Console.WriteLine(result.ToString());
